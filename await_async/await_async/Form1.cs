7dea3ab1b73257c3d744ac43af11fd1cb893988e
Norbert <54501347+norbertpocztarek@users.noreply.github.com>
2023-02-19 19:49:31 +0100

    First example

--- await_async/await_async/Form1.cs
+++ await_async/await_async/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -15,6 +16,31 @@ namespace await_async
         public Form1()
         {
             InitializeComponent();
+        }
+
+        protected async void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("button1_click: Początek");
+            Task<long> zadanie = DoSomethingAsync("async/await");
+            MessageBox.Show("Akcja została uruchomiona");
+            long wynik = await zadanie;
+            MessageBox.Show("Wynik: " + wynik);
+            MessageBox.Show("button1_click: Koniec");
+        }
+
+        Task<long> DoSomethingAsync(object argument) 
+        {
+            Func<object, long> akcja =
+                (object _argument) =>
+                {
+                    MessageBox.Show("Akcja: Początek, argument: " + _argument.ToString());
+                    Thread.Sleep(10000);
+                    MessageBox.Show("Akcja: Koniec");
+                    return DateTime.Now.Ticks;
+                };
+            Task<long> zadanie = new Task<long>(akcja, argument);
+            zadanie.Start();
+            return zadanie;
         }
     }
 }
